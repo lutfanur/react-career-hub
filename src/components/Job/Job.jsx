f2b0d773b1518230e4aa63b36503ebb9df060bfd
Lutfaakter <lutfanur20@gmail.com>
2024-03-23 13:16:50 +0000

    load job details page and display

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,8 +1,9 @@
 import { MdOutlineLocationOn } from "react-icons/md";
 import { HiOutlineCurrencyDollar } from "react-icons/hi2";
+import { Link } from "react-router-dom";
 
 const Job = ({ job }) => {
-    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+    const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Shoes" /></figure>
@@ -14,11 +15,13 @@ const Job = ({ job }) => {
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
                 </div>
                 <div className="mt-4 flex">
-                <h2 className="flex mr-4"><MdOutlineLocationOn className="text-2xl mr-2" />{location}</h2>
-                <h2 className="flex"><HiOutlineCurrencyDollar className="text-2xl mr-2"/> {salary}</h2>
+                    <h2 className="flex mr-4"><MdOutlineLocationOn className="text-2xl mr-2" />{location}</h2>
+                    <h2 className="flex"><HiOutlineCurrencyDollar className="text-2xl mr-2" /> {salary}</h2>
                 </div>
                 <div className="card-actions">
-                    <button className="btn btn-primary">View Details</button>
+                    <Link to={`/job/${id}`}>
+                        <button className="btn btn-primary">View Details</button>
+                    </Link>
                 </div>
             </div>
         </div>
